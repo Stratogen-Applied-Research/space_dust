2ae974e7a48d68cbc69c650b874df889b7057256
jmcguigs <jemcguigan@outlook.com>
2024-12-11 21:28:06 -0800

    refactor julianDateToDateTime

--- lib/time/time_conversions.ex
+++ lib/time/time_conversions.ex
@@ -6,8 +6,11 @@ defmodule SpaceDust.Time.TimeConversions do
   def julianDateToDateTime(julianDate) do
     # convert the Julian date to a Unix timestamp
     unixTimestamp = (julianDate - Constants.unixEpochJulianDate) * Constants.secondsPerDay
+    unixSeconds = trunc(unixTimestamp)
+    unixMicroseconds = trunc((unixTimestamp - unixSeconds) * 1_000_000)
     # convert the Unix timestamp to a DateTime
-    DateTime.from_unix(unixTimestamp)
+    {:ok, dateTimeToSecond} = DateTime.from_unix(unixSeconds, :second)
+    DateTime.add(dateTimeToSecond, unixMicroseconds, :microsecond)
   end
 
   @doc "convert a DateTime to a Julian date"
